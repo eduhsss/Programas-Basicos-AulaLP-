a6acade3bbaab92a219cebc108bd9c3c25f1f0a9
Eduhss <47652040+Eduhss@users.noreply.github.com>
2019-02-15 02:09:13 -0200

    Add files via upload

---
+++ 2/src/perimetroretangulo/PerimetroRetangulo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perimetroretangulo;
+
+/**
+ *
+ * @author Lab3PC20
+ */
+public class PerimetroRetangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PerimetroRetângulo ca = new PerimetroRetângulo();
+            ca.setVisible(true);
+    }
+    
+}
